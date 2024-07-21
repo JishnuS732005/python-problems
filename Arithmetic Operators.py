0883f13160ad07e027a9c103a387b49c732052e4
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-21 19:53:11 +0530

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,11 @@
+#  https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if _name_ == '_main_':
+    a = int(input())
+    b = int(input())
+    sum = a+b
+    diff = a-b
+    mul = a*b
+    print(sum)
+    print(diff)
+    print(mul)

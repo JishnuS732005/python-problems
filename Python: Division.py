575c3db78d58d1d5e17146774b279accc319e240
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-21 19:53:41 +0530

    Create Python: Division.py

---
+++ Python: Division.py
@@ -0,0 +1,9 @@
+#  https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+
+if _name_ == '_main_':
+    a = int(input())
+    b = int(input())
+    int_div = a // b
+    float_div = a / b
+    print(int_div)
+    print(float_div)

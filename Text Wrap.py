6b03b6e902f09a974cdde707ea956286003ef196
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-22 21:45:15 +0530

    Create Text Wrap.py

---
+++ Text Wrap.py
@@ -0,0 +1,11 @@
+#  https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+
+import textwrap
+
+def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+if _name_ == '_main_':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
